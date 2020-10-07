26d7ac62d857c0f91304023061928a1f270d2d43
Vitaliy Taran <vitaliy.t@develops.today>
2020-10-07 10:35:51 +0300

    feat(locale): add locale

---
+++ src/locale/index.ts
@@ -0,0 +1,18 @@
+enum locale {
+  back = 'Назад',
+  moneyTransfers = 'Грошові перекази',
+  transferAmount = 'Сума переказу',
+  close = 'Закрити',
+  anotherBankCard = 'На карту іншого банку',
+  doPayCommission = 'Сплатити комісію з одержувача 24.5 грн?',
+  secureTransferTitle = 'Підтвердження операції',
+  secureMessage = 'Введіть пароль з СМС, що відправлено на Ваш номер',
+  secureMessageSecond = 'Ніколи не повідомляйте його іншим людям',
+  verifyMessage = 'пароль з СМС',
+  sendMoney = 'переказати',
+  onward = 'далі',
+  success = 'Успішно!',
+  result = 'Результат',
+}
+
+export default locale;
